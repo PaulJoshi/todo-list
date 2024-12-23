1afdbcf8aa3e855415d72b8ab1c004de6b725bde
Paul Joshi <pauljoshi@gmail.com>
2024-12-23 20:21:49 +0530

    Day 2 Vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,26 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Card from './components/Card';
-import Input from './components/Input';
-
-const initialTasks = [
-  {
-    id: 0,
-    text: "This is my first task",
-  },
-  {
-    id: 1,
-    text: "This is second task",
-  },
-  {
-    id: 2,
-    text: "Learn React",
-  }
-]
+import Tasks from './components/Tasks';
+import About from './components/About';
+import Header from './components/Header';
 
 function App() {
 
-  const [tasks, setTasks] = useState(initialTasks)
-
-  const addTaskHandler = (enteredTask) => {
-    setTasks(previousTasks => [
-      ...previousTasks,
-      {
-        id: Math.random(),
-        text: enteredTask,
-      }
-    ])
-  }
-
-  const deleteTaskHandler = (taskId) => {
-    setTasks(previousTasks => previousTasks.filter(item => item.id !== taskId))
-  }
-
   return (
-    <div className="App">
+    <BrowserRouter>
+      <div className="App">
 
-      <header>
-        <h1>My React App</h1>
-        <h2>This is my first React App</h2>
-      </header>
+        <Header />
 
-      <main>
+        <main>
+          <Routes>
+            <Route path='/' element={<Tasks />} />
+            <Route path='/about' element={<About />} />
+          </Routes>
+        </main>
 
-        <div id="tasks">
-
-          <Input onAddTask={addTaskHandler} />
-          {
-            tasks.map(item => (
-              <Card key={item.id} task={item} onDeleteTask={deleteTaskHandler} />
-            ))
-          }
-        </div>
-
-      </main>
-
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom"
+
+const Header = () => {
+	return (
+		<header>
+			<h1>My React App</h1>
+			<h2>This is my first React App</h2>
+
+			<nav className="navbar">
+				<Link to="/">Home</Link>
+				<Link to="/about">About</Link>
+			</nav>
+
+		</header>
+	)
+}
+
+export default Header
